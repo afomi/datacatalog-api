ccd80fced9cd6be00dc1663240fafb6fb5d253af
David James <djames@sunlightfoundation.com>
2009-07-31 03:26:26 -0400

    Refactored to take advantage of Status40x

--- test/helpers/shared/api_key_not_allowed.rb
+++ test/helpers/shared/api_key_not_allowed.rb
@@ -1,9 +1,14 @@
+# Please note that this is intentionally *not* an authentication
+# error. It is the result of passing an API key when it is not
+# allowed. In order words, some actions can only be done
+# anonymously.
+
 module ApiKeyNotAllowed
 
   def self.included(mod)
-    # should_give Status400
+    mod.should_give Status400
     
-    mod.test "body should explain the problem" do
+    mod.test "body should say the API key is an invalid param" do
       assert_include "errors", parsed_response_body
       assert_include "invalid_params", parsed_response_body["errors"]
       assert_include "api_key", parsed_response_body["errors"]["invalid_params"]
--- test/helpers/shared/missing_api_key.rb
+++ test/helpers/shared/missing_api_key.rb
@@ -1,9 +1,7 @@
 module MissingApiKey
   
   def self.included(mod)
-    mod.test "status should be 401 Not Authorized" do
-      assert_equal 401, last_response.status
-    end
+    mod.should_give Status401
 
     mod.test "body should say the API key is missing" do
       assert_include "errors", parsed_response_body
