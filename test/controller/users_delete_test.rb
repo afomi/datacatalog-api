9f186a21a13661a98f0cead6eb49dfa1701cad74
David James <djames@sunlightfoundation.com>
2009-07-31 16:35:02 -0400

    Updated users_delete_test so that it passes.

--- test/controller/users_delete_test.rb
+++ test/controller/users_delete_test.rb
@@ -3,19 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 class DeleteUsersControllerTest < RequestTestCase
 
   def setup_for_deletion
-    User.create({
-      :_id     => "51515",
+    user = User.create({
       :name    => "Will Not-Last-Long",
       :email   => "will.not.last.long@email.com",
       :purpose => "User account for Web application"
     })
+    @id = user.id
     @user_count = User.count
   end
   
   context "anonymous user : delete /users" do
     before :all do
       setup_for_deletion
-      delete '/users/51515'
+      delete "/users/#{@id}"
     end
     
     should_give MissingApiKey
@@ -28,11 +28,11 @@ class DeleteUsersControllerTest < RequestTestCase
   context "incorrect user : delete /users" do
     before :all do
       setup_for_deletion
-      delete '/users/51515', :api_key => "does_not_exist_in_database"
+      delete "/users/#{@id}", :api_key => "does_not_exist_in_database"
     end
     
     should_give InvalidApiKey
-
+  
     test "should not change user count" do
       assert_equal @user_count, User.count
     end
@@ -41,11 +41,11 @@ class DeleteUsersControllerTest < RequestTestCase
   context "unconfirmed user : delete /users" do
     before :all do
       setup_for_deletion
-      delete '/users/51515', :api_key => @unconfirmed_user.api_key
+      delete "/users/#{@id}", :api_key => @unconfirmed_user.api_key
     end
     
     should_give UnauthorizedApiKey
-
+  
     test "should not change user count" do
       assert_equal @user_count, User.count
     end
@@ -54,11 +54,11 @@ class DeleteUsersControllerTest < RequestTestCase
   context "confirmed user : delete /users" do
     before :all do
       setup_for_deletion
-      delete '/users/51515', :api_key => @confirmed_user.api_key
+      delete "/users/#{@id}", :api_key => @confirmed_user.api_key
     end
     
     should_give UnauthorizedApiKey
-
+  
     test "should not change user count" do
       assert_equal @user_count, User.count
     end
@@ -67,14 +67,14 @@ class DeleteUsersControllerTest < RequestTestCase
   context "admin user : delete /users" do
     before :all do
       setup_for_deletion
-      delete '/users/51515', :api_key => @admin_user.api_key
+      delete "/users/#{@id}", :api_key => @admin_user.api_key
     end
     
     should_give Status200
-
+  
     test "body should have correct name value" do
       assert_include "_id", parsed_response_body
-      assert_equal "51515", parsed_response_body["_id"]
+      assert_equal @id, parsed_response_body["_id"]
     end
     
     test "should decrement user count" do
