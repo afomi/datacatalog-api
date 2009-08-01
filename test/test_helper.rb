47481043344a971c0ff8d6af7e28552943d60587
David James <djames@sunlightfoundation.com>
2009-07-31 20:11:10 -0400

    Removed full database reset. See comment.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,3 +17,12 @@ require_dir 'test/helpers/assertions'
 require_dir 'test/helpers/shared'
 
 set :environment, :test
+
+# It is safer to reset the database before running any tests.
+# If you want to do so, uncomment the following line.
+#
+# MongoMapper.connection.drop_database MongoMapper.database.name
+#
+# However, it slows things down considerably (by several seconds
+# on my system). The only time I've had to worry about it is when
+# I've upgraded my versions of MongoMapper.
