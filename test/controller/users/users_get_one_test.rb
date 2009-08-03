f09739e4eb345aec1392800f1786bc43b3e214d5
David James <djames@sunlightfoundation.com>
2009-08-03 16:26:33 -0400

    Split users_get_test into two parts

--- test/controller/users/users_get_one_test.rb
+++ test/controller/users/users_get_one_test.rb
@@ -1,75 +1,51 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
 
-class GetUsersControllerTest < RequestTestCase
+class UsersGetOneControllerTest < RequestTestCase
+
+  def create_example_user
+    User.create({
+      :name    => "Example User",
+      :email   => "example.user@email.com",
+      :purpose => "User account for Web application"
+    })
+  end
   
-  context "anonymous user : get /users" do
+  context "anonymous user : get /users/:id" do
     before :all do
-      get '/users'
+      @id = create_example_user.id
+      get "/users/#{@id}"
     end
     
     use "return 401 because the API key is missing"
   end
   
-  context "incorrect user : get /users" do
+  context "incorrect user : get /users/:id" do
     before :all do
-      get '/users', :api_key => "does_not_exist_in_database"
+      @id = create_example_user.id
+      get "/users/#{@id}", :api_key => "does_not_exist_in_database"
     end
     
     use "return 401 because the API key is invalid"
   end
   
-  context "unconfirmed user : get /users" do
+  context "unconfirmed user : get /users/:id" do
     before :all do
-      get '/users', :api_key => @unconfirmed_user.api_key
+      @id = create_example_user.id
+      get "/users/#{@id}", :api_key => @unconfirmed_user.api_key
     end
     
     use "return 401 because the API key is unauthorized"
   end
   
-  context "confirmed user : get /users" do
+  context "confirmed user : get /users/:id" do
     before :all do
-      get '/users', :api_key => @confirmed_user.api_key
+      @id = create_example_user.id
+      get "/users/#{@id}", :api_key => @confirmed_user.api_key
     end
     
     use "return 401 because the API key is unauthorized"
   end
-  
-  context "admin user : get /users" do
-    before :all do
-      get '/users', :api_key => @admin_user.api_key
-    end
-    
-    use "return 200 Ok"
-    
-    test "body should have 3 top level elements" do
-      assert_equal 3, parsed_response_body.length
-    end
-  
-    test "body should have correct name" do
-      assert_equal "Admin", parsed_response_body[0]["name"]
-    end
-        
-    test "body should have correct email" do
-      assert_equal "admin@inter.net", parsed_response_body[0]["email"]
-    end
-        
-    test "body should have created_at" do
-      assert_include "created_at", parsed_response_body[0]
-    end
-        
-    test "body should have updated_at" do
-      assert_include "updated_at", parsed_response_body[0]
-    end
-  
-    test "body should have id" do
-      assert_include "id", parsed_response_body[0]
-    end
-        
-    test "body should not have _id" do
-      assert_not_include "_id", parsed_response_body[0]
-    end
-  end
-  
+
   context "admin user : get /users/:fake_id : not found" do
     before :all do
       @fake_id = get_fake_mongo_object_id
