d3b0879c1875617ad8f25da487cfc243c3869366
David James <djames@sunlightfoundation.com>
2009-08-25 15:15:43 -0400

    whitespace

--- models/api_key.rb
+++ models/api_key.rb
@@ -2,9 +2,9 @@ class ApiKey
   
   include MongoMapper::EmbeddedDocument
   
-  key :api_key, String
-  key :key_type, String
-  key :purpose, String
+  key :api_key,    String
+  key :key_type,   String
+  key :purpose,    String
   key :created_at, Time
 
   def initialize(attrs={})
@@ -13,7 +13,7 @@ class ApiKey
   end
 
   alias original_to_json to_json
-  def to_json(options = nil)
+  def to_json(options=nil)
     original_to_json({
       :methods => :id,
       :except  => :_id
