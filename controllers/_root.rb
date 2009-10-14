577cab522631d0ccd97cd7dff991d42337b8d408
David James <djames@sunlightfoundation.com>
2009-10-14 11:52:47 -0400

    Bump API version to 0.3.0

--- controllers/_root.rb
+++ controllers/_root.rb
@@ -4,7 +4,7 @@ module DataCatalog
     
     PROJECT_META_DATA = {
       "name"    => "National Data Catalog API",
-      "version" => "0.2.4",
+      "version" => "0.3.0",
       "resource_directory" => {
         "href" => "/resources"
       },
