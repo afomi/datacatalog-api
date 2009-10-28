2de5fb9f81bfd5a078f9c3687b4e5acb67ee73ca
David James <djames@sunlightfoundation.com>
2009-10-28 11:11:52 -0400

    Removed note about 'crack' vs. 'json' gem

--- test/helpers/test_helpers/request_helpers.rb
+++ test/helpers/test_helpers/request_helpers.rb
@@ -5,17 +5,3 @@ module RequestHelpers
   end
   
 end
-
-# ===== A note on JSON parsing =====
-#
-# I gave up on using this gem:
-#   http://gemcutter.org/gems/json
-#
-# Because it cannot parse 'null' or '1':
-#
-#   > JSON.parse('null')
-#   JSON::ParserError: 618: unexpected token at 'null'
-#
-#   > JSON.parse('1')
-#   JSON::ParserError: A JSON text must at least contain two octets!
-#
