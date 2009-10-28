bbfa0aae30c28c410963e271c3abdb481b8f1715
David James <djames@sunlightfoundation.com>
2009-10-28 11:11:25 -0400

    Removed old comment

--- test/helpers/test_helpers/request_helpers.rb
+++ test/helpers/test_helpers/request_helpers.rb
@@ -1,14 +1,5 @@
 module RequestHelpers
 
-  # Beware -- although caching (memoizing) is tempting:
-  #
-  #   @parsed_response_body ||= JSON.parse(last_response.body)
-  #
-  # But this is dangerous; when calling multiple REST methods,
-  # the later calls would not get an updated parse.
-  #
-  # To allow for this, we'd need to be able to clear the cached
-  # result -- that seems like more trouble than it is worth.
   def parsed_response_body
     Crack::JSON.parse(last_response.body)
   end
