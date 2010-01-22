22fa332504d793d840b080145c088382a883d657
Luigi Montanez <luigi.montanez@gmail.com>
2010-01-22 17:54:30 -0500

    Capistrano deployment [Story2231066]

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+set :environment, (ENV['target'] || 'production')
+
+set :user, 'datcat'
+set :application, 'datacatalog-api'
+set :domain, 'api.nationaldatacatalog.com'
+
+set :scm, :git
+set :repository, "git://github.com/sunlightlabs/#{application}.git"
+set :branch, 'master'
+
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/www/#{application}"
+set :deploy_via, :remote_cache
+set :runner, user
+set :admin_runner, runner
+
+role :app, domain
+role :web, domain
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :migrate do; end
+  
+  desc "Restart the server"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join current_path, 'tmp', 'restart.txt'}"
+  end
+  
+  desc "Get shared files into position"
+  task :after_update_code, :roles => [:web, :app] do
+    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+    run "ln -nfs #{shared_path}/config.ru #{release_path}/config.ru"
+    run "rm #{File.join release_path, 'tmp', 'pids'}"
+    run "rm #{File.join release_path, 'public', 'system'}"
+    run "rm #{File.join release_path, 'log'}"
+  end
+end
