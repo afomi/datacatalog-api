1a9b6994bcea342046b7143e9f248f2437cb6c5b
David James <djames@sunlightfoundation.com>
2010-02-16 16:37:37 -0500

    ensure_indexes! not supported [#2509837]

--- app.rb
+++ app.rb
@@ -14,5 +14,3 @@ Dir.glob(base + '/model_helpers/*.rb'     ).each { |f| require f }
 Dir.glob(base + '/models/*.rb'            ).each { |f| require f }
 Dir.glob(base + '/resource_helpers/*.rb'  ).each { |f| require f }
 Dir.glob(base + '/resources/*.rb'         ).each { |f| require f }
-
-MongoMapper.ensure_indexes!
