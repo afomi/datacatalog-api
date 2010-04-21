ce212718589cd407828370bb97796e489336ad0b
David James <djames@sunlightfoundation.com>
2010-04-21 10:53:39 -0400

    Add Import model and test

---
+++ models/import.rb
@@ -0,0 +1,49 @@
+class Import
+
+  include MongoMapper::Document
+
+  # == Attributes
+
+  key :importer_id,  Mongo::ObjectID
+  key :status,       String # see STATUS_TYPES below
+  key :start_time,   Time
+  key :finish_time,  Time
+  timestamps!
+
+  # == Indices
+  
+  ensure_index :importer_id
+  ensure_index :finish_time
+
+  # == Associations
+  
+  belongs_to :importer
+  
+  protected
+
+  # == Validations
+
+  validates_presence_of :name
+  validates_presence_of :start_time
+  validates_presence_of :finish_time
+
+  STATUS_TYPES = %w(success failure)
+
+  validate :validate_status
+  def validate_status
+    unless STATUS_TYPES.include?(status)
+      errors.add(:status, "must be one of: #{STATUS_TYPES.join(', ')}")
+    end
+  end
+
+  # == Class Methods
+
+  # == Various Instance Methods
+  
+  public
+  
+  def duration
+    finish_time - start_time
+  end
+
+end
---
+++ test/unit/import_test.rb
@@ -0,0 +1,116 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_unit_helper')
+
+class ImportUnitTest < ModelTestCase
+  
+  shared "valid Import" do
+    test "should be valid" do
+      assert_equal true, @import.valid?
+    end
+  end
+
+  shared "invalid Import" do
+    test "should not be valid" do
+      assert_equal false, @import.valid?
+    end
+  end
+
+  shared "import.name can't be empty" do
+    test "should have empty error on name" do
+      @import.valid?
+      assert_include :name, @import.errors.errors
+      assert_include "can't be empty", @import.errors.errors[:name]
+    end
+  end
+
+  shared "import.start_time can't be empty" do
+    test "should have empty error on start_time" do
+      @import.valid?
+      assert_include :start_time, @import.errors.errors
+      assert_include "can't be empty", @import.errors.errors[:start_time]
+    end
+  end
+
+  shared "import.finish_time can't be empty" do
+    test "should have empty error on finish_time" do
+      @import.valid?
+      assert_include :finish_time, @import.errors.errors
+      assert_include "can't be empty", @import.errors.errors[:finish_time]
+    end
+  end
+
+  shared "import.status must be valid" do
+    test "should have error on status" do
+      @import.valid?
+      assert_include :status, @import.errors.errors
+      assert_include "must be one of: success, failure",
+        @import.errors.errors[:status]
+    end
+  end
+
+  # - - - - - - - - - -
+  
+  context "Import : source" do
+    before do
+      finish = Time.now
+      @valid_params = {
+        :name        => "data.gov",
+        :start_time  => finish - 30,
+        :finish_time => finish,
+        :status      => 'success',
+      }
+    end
+    
+    after do
+    end
+    
+    context "correct params" do
+      before do
+        @import = Import.new(@valid_params)
+      end
+      
+      use "valid Import"
+      
+      test "calculate correct duration" do
+        assert_equal 30, @import.duration
+      end
+    end
+
+    context "missing name" do
+      before do
+        @import = Import.new(@valid_params.merge(:name => nil))
+      end
+      
+      use "invalid Import"
+      use "Import.name can't be empty"
+    end
+
+    context "missing start_time" do
+      before do
+        @import = Import.new(@valid_params.merge(:start_time => nil))
+      end
+      
+      use "invalid Import"
+      use "Import.start_time can't be empty"
+    end
+
+    context "missing finish_time" do
+      before do
+        @import = Import.new(@valid_params.merge(:finish_time => nil))
+      end
+      
+      use "invalid Import"
+      use "Import.finish_time can't be empty"
+    end
+    
+    context "invalid status" do
+      before do
+        @import = Import.new(@valid_params.merge(:status => ""))
+      end
+      
+      use "invalid Import"
+      use "import.status must be valid"
+    end
+    
+  end
+  
+end
