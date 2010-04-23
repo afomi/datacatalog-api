98e754c073adf1a43171ed32ad8916df3a12a9ae
David James <djames@sunlightfoundation.com>
2010-04-23 10:45:22 -0400

    Added GET tests for /imports and /importers

---
+++ test/resource/importers/importers_get_one_test.rb
@@ -0,0 +1,29 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../test_resource_helper')
+
+class ImportersGetOneTest < RequestTestCase
+
+  def app; DataCatalog::Importers end
+
+  before do
+    @importer = create_importer({
+      :name => "hawaii.gov"
+    })
+  end
+
+  after do
+    @importer.destroy
+  end
+  
+  context "normal API key : get /:id" do
+    before do
+      get "/#{@importer.id}", :api_key => @curator_user.primary_api_key
+    end
+    
+    use "return 200 Ok"
+  
+    test "body should have correct text" do
+      assert_equal "hawaii.gov", parsed_response_body["name"]
+    end
+  end
+
+end
---
+++ test/resource/imports/imports_get_one_test.rb
@@ -0,0 +1,33 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../test_resource_helper')
+
+class ImportsGetOneTest < RequestTestCase
+
+  def app; DataCatalog::Imports end
+
+  before do
+    @importer = create_importer({
+      :name => "hawaii.gov"
+    })
+    @import = create_import({
+      :importer_id => @importer.id
+    })
+  end
+  
+  after do
+    @import.destroy
+    @importer.destroy
+  end
+
+  context "normal API key : get /:id" do
+    before do
+      get "/#{@import.id}", :api_key => @curator_user.primary_api_key
+    end
+
+    use "return 200 Ok"
+  
+    test "body should have correct text" do
+      assert_equal @importer.id.to_s, parsed_response_body["importer_id"]
+    end
+  end
+
+end
