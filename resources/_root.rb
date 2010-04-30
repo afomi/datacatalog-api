29acebf3ebc1b28cc47eca4775d0a4e44fdd15db
David James <djames@sunlightfoundation.com>
2010-04-30 13:05:57 -0400

    Version 0.4.9

--- resources/_root.rb
+++ resources/_root.rb
@@ -5,7 +5,7 @@ module DataCatalog
     
     META = {
       "name"    => "National Data Catalog API",
-      "version" => "0.4.8",
+      "version" => "0.4.9",
       "resource_directory" => {
         "href" => "/resources"
       },
