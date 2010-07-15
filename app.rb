b00095340c8c558f345ade27f21752781cee432b
David James <djames@sunlightfoundation.com>
2010-07-15 11:13:25 -0400

    minor: whitespace

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ Sinatra::Base.set(:config, Config.environment_config)
 Config.setup
 
 base = File.dirname(__FILE__)
-Dir.glob(base + '/lib/*.rb'               ).each { |f| require f }
-Dir.glob(base + '/model_helpers/*.rb'     ).each { |f| require f }
-Dir.glob(base + '/models/*.rb'            ).each { |f| require f }
-Dir.glob(base + '/resource_helpers/*.rb'  ).each { |f| require f }
-Dir.glob(base + '/resources/*.rb'         ).each { |f| require f }
+Dir.glob(base + '/lib/*.rb'             ).each { |f| require f }
+Dir.glob(base + '/model_helpers/*.rb'   ).each { |f| require f }
+Dir.glob(base + '/models/*.rb'          ).each { |f| require f }
+Dir.glob(base + '/resource_helpers/*.rb').each { |f| require f }
+Dir.glob(base + '/resources/*.rb'       ).each { |f| require f }
