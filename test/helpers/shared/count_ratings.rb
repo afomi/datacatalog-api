a1af40d4cdbdc371225441ecbf80679108d6dd10
David James <djames@sunlightfoundation.com>
2010-07-20 12:29:00 -0400

    MongoMapper equality fix
    Best I can tell, MongoMapper still does not get equality right.
    
    Maybe one day it will? Until then I can only point to my rants:
    http://djwonk.com/blog/2009/10/21/broken-equality-in-mongomapper/
    http://groups.google.com/group/mongomapper/browse_thread/thread/85af8026a2dba34d

--- test/helpers/shared/count_ratings.rb
+++ test/helpers/shared/count_ratings.rb
@@ -20,10 +20,17 @@ class RequestTestCase
   
   shared "rating unchanged" do
     test "should not change rating in database" do
-      assert_equal @rating_copy, Rating.find_by_id(@rating.id)
-      # TODO: use reload
+      # Worked in MongoMapper 0.7.x
+      # assert_equal @rating_copy, Rating.find_by_id(@rating.id)
+
+      original = Rating.find_by_id(@rating.id)
+      assert_equal @rating_copy.kind,       original.kind
+      assert_equal @rating_copy.user_id,    original.user_id
+      assert_equal @rating_copy.source_id,  original.source_id
+      assert_equal @rating_copy.comment_id, original.comment_id
+      assert_equal @rating_copy.value,      original.value
+      assert_equal @rating_copy.text,       original.text
     end
   end
-  
   
 end
